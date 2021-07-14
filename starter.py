2b39205e40cb3c669608a9f998514c77d946f5c8
Tad Thorley <phaedryx@gmail.com>
2021-07-14 14:07:26 -0600

    fixed

--- starter.py
+++ starter.py
@@ -117,29 +117,26 @@ class Interpreter:
     def current_scope(self):
         return(self.stack[-1])
 
-    def add(x, y):
-        return x+y
+    def add(self, x, y):
+        return x + y
 
-    def subtract(x, y):
-        return x-y
+    def subtract(self, x, y):
+        return x - y
 
-    def multiply(x, y):
-        return x*y
+    def multiply(self, x, y):
+        return x * y
 
-    def divide(x, y):
-        return x/y
+    def divide(self, x, y):
+        return x / y
 
-    def exponentiate(x, y):
-        return x**y
+    def exponentiate(self, x, y):
+        return x ** y
 
-    def mod(x, y):
-        return x%y
+    def mod(self, x, y):
+        return x % y
 
     def start_function(self, name, **parameter_list):
         pass
-
-# # dictionary of names (for storing variables)
-# names = { }
 
 interpreter = Interpreter()
 
