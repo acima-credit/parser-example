3dd8e65c2bc6b42862d8ff1a6852886095f76649
Tad Thorley <phaedryx@gmail.com>
2021-07-14 14:00:42 -0600

    borked, but close

--- starter.py
+++ starter.py
@@ -23,13 +23,13 @@ tokens = (
 
 # Tokens
 t_FUNCTION = r'~\>'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_ASSIGNMENT     = r'[=]'
 t_ADDITION       = r'[+]'
 t_SUBTRACTION    = r'[-]'
 t_MULTIPLICATION = r'[*]'
 t_DIVISION       = r'[/]'
-t_EXPONENTIATION = r'[^]'
+t_EXPONENTIATION = r'\^'
 t_MODULO         = r'[%]'
 t_LPAREN         = r'[(]'
 t_RPAREN         = r'[)]'
@@ -41,6 +41,7 @@ def t_NUMBER(t):
 
 # Ignored characters
 t_ignore = " \t"
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
@@ -60,26 +61,105 @@ precedence = (
     ('left', 'EXPONENTIATION')
 )
 
-# dictionary of names (for storing variables)
-names = { }
+class Scope:
+    def __init__(self):
+        self.names = {}
+        self.loop_counter = 0
+        self.loop_statements = []
+
+class Interpreter:
+    def __init__(self):
+        self.stack = []
+        self.push_scope()
+
+    def eval(self, node):
+        operation = node[0]
+
+        if operation == "eval":
+            return self.eval(node[1])
+        elif operation == "assign":
+            return self.assign_value(node[1], self.eval(node[2]))
+        elif operation == "number":
+            return node[1]
+        elif operation == "lookup":
+            return self.variable_value(node[1])
+        elif operation == "add":
+            return self.add(self.eval(node[1]), self.eval(node[2]))
+        elif operation == 'subtract':
+            return self.subtract(self.eval(node[1]), self.eval(node[2]))
+        elif operation == 'multiply':
+            return self.multiply(self.eval(node[1]), self.eval(node[2]))
+        elif operation == 'divide':
+            return self.divide(self.eval(node[1]), self.eval(node[2]))
+        elif operation == 'exponentiate':
+            return self.exponentiate(self.eval(node[1]), self.eval(node[2]))
+        elif operation == 'mod':
+            return self.mod(self.eval(node[1]), self.eval(node[2]))
+
+    def assign_value(self, name, value):
+        self.current_scope().names[name] = value
+        return value
+
+    def expression_number(self, number):
+        return(number)
+
+    def variable_value(self, name):
+        try:
+            return(self.current_scope().names[name])
+        except LookupError:
+            print(f"Undefined name {name}")
+            return None
+
+    def push_scope(self):
+        scope = Scope()
+        self.stack.append(scope)
+
+    def current_scope(self):
+        return(self.stack[-1])
+
+    def add(x, y):
+        return x+y
+
+    def subtract(x, y):
+        return x-y
+
+    def multiply(x, y):
+        return x*y
+
+    def divide(x, y):
+        return x/y
+
+    def exponentiate(x, y):
+        return x**y
+
+    def mod(x, y):
+        return x%y
+
+    def start_function(self, name, **parameter_list):
+        pass
+
+# # dictionary of names (for storing variables)
+# names = { }
+
+interpreter = Interpreter()
 
 def p_statement_assign(p):
     'statement : NAME ASSIGNMENT expression'
-    names[p[1]] = p[3]
+    p[0] = ('assign', p[1], p[3])
 
 def p_statement_expr(p):
     'statement : expression'
-    print(p[1])
+    p[0] = ('eval', p[1])
 
 def p_expression_number(p):
     'expression : NUMBER'
-    p[0] = p[1]
+    p[0] = ('number', p[1])
 
 def p_expression_name(p):
     'expression : NAME'
-    if p[1] == 'q' or p[1] == 'quit': quit()
+    if p[1] == 'q' or p[1] == 'quit' or p[1] == 'exit': quit()
     try:
-        p[0] = names[p[1]]
+        p[0] = ('lookup', p[1])
     except LookupError:
         print(f"Undefined name {p[1]!r}")
         p[0] = 0
@@ -89,38 +169,38 @@ def p_error(p):
 
 def p_expression_parentheses(p):
     'expression : LPAREN expression RPAREN'
-    p[0] = p[2]
+    p[0] = ('eval', p[2])
 
 def p_lambda(p):
     'expression : NAME LPAREN NAME RPAREN FUNCTION expression'
-    names[p[1]] = f'{p[6]}'
+    # names[p[1]] = f'{p[6]}'
 
 def p_expression_addition(p):
     'expression : expression ADDITION expression'
-    p[0] = p[1] + p[3]
+    p[0] = ('add', p[1], p[3])
 
 def p_expression_subtraction(p):
     'expression : expression SUBTRACTION expression'
-    p[0] = p[1] - p[3]
+    p[0] = ('subtract', p[1], p[3])
 
 def p_expression_multiplication(p):
     'expression : expression MULTIPLICATION expression'
-    p[0] = p[1] * p[3]
+    p[0] = ('multiply', p[1], p[3])
 
 def p_expression_division(p):
     'expression : expression DIVISION expression'
     try:
-        p[0] = p[1] / p[3]
+        p[0] = ('divide', p[1], p[3])
     except ZeroDivisionError:
         print('Division by Zero')
 
 def p_expression_exponentiation(p):
     'expression : expression EXPONENTIATION expression'
-    p[0] = p[1] ** p[3]
+    p[0] = ('exponentiate', p[1], p[3])
 
 def p_expression_modulo(p):
     'expression : expression MODULO expression'
-    p[0] = p[1] % p[3]
+    p[0] = ('mod', p[1], p[3])
 
 import ply.yacc as yacc
 yacc.yacc()
@@ -130,4 +210,7 @@ while True:
         s = input('calc> ')
     except EOFError:
         break
-    yacc.parse(s)
+    parse_tree = yacc.parse(s)
+    print(parse_tree)
+    result = interpreter.eval(parse_tree)
+    print(result)
